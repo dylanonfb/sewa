f2c76f6fed8071a7bf793fca1ec37c8b0cd21534
Dylan <dylanonfb@gmail.com>
2022-05-25 23:50:31 +0530

    social media link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -557,9 +557,9 @@ The thoughts and the dilemmas that we encountered during that time led to the ge
                               </div> 
 
                               <ul class="social-icon">
-                                   <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
+                                   <li><a href="https://www.facebook.com/Sewahealthlink-100901205957607" class="fa fa-facebook-square" attr="facebook icon"></a></li>
                                    <li><a href="#" class="fa fa-twitter"></a></li>
-                                   <li><a href="#" class="fa fa-instagram"></a></li>
+                                   <li><a href="#" class="https://www.instagram.com/sewahealth"></a></li>
                               </ul>
                          </div>
                     </div>
