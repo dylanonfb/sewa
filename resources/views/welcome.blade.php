b01dbe609a675c300d3d0bb58dabcec72e9eb7e5
Dylan <dylanonfb@gmail.com>
2022-05-25 23:35:19 +0530

    hospice below services

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -140,16 +140,7 @@ http://www.tooplate.com/view/2098-health
           </div>
      </section>
 
-     <section id="hospice">
-          <div class="container">
-               <div class="row">
-                    <div class="col-md12 col-sm-12">
-                         <h2>Hospice Care</h2>
-                         <p>Hospice care is a type of health care that focuses on the palliation of a terminally ill patient's pain and symptoms and attending to their emotional and spiritual needs at the end of life. Hospice care prioritizes comfort and quality of life by reducing pain and suffering. Hospice care provides an alternative to therapies focused on life-prolonging measures that may be arduous, likely to cause more symptoms, or are not aligned with a person's goals.</p>
-                    </div>
-               </div>
-          </div>
-     </section>
+   
 
      <!-- NEWS -->
      <section id="news" data-stellar-background-ratio="2.5">
@@ -320,7 +311,16 @@ http://www.tooplate.com/view/2098-health
               </div>
           </div>
      </section>
-
+     <section id="hospice" style="padding-top:100px;padding-bottom:100px;">
+          <div class="container">
+               <div class="row">
+                    <div class="col-md12 col-sm-12">
+                         <h2>Hospice Care</h2>
+                         <p>Hospice care is a type of health care that focuses on the palliation of a terminally ill patient's pain and symptoms and attending to their emotional and spiritual needs at the end of life. Hospice care prioritizes comfort and quality of life by reducing pain and suffering. Hospice care provides an alternative to therapies focused on life-prolonging measures that may be arduous, likely to cause more symptoms, or are not aligned with a person's goals.</p>
+                    </div>
+               </div>
+          </div>
+     </section>
      <!-- ABOUT -->
      <section id="about">
           <div class="container">
